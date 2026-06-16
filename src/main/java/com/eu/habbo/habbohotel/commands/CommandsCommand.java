883c39b3255813ae62873fcd8914de9b4577ba0e
Vixito <carlosandres81182@gmail.com>
2026-06-16 13:34:50 -0500

    refactor: categorize command list by minimum rank and include command names in output

--- src/main/java/com/eu/habbo/habbohotel/commands/CommandsCommand.java
+++ src/main/java/com/eu/habbo/habbohotel/commands/CommandsCommand.java
@@ -14,10 +14,35 @@ public class CommandsCommand extends Command {
     public boolean handle(GameClient gameClient, String[] params) throws Exception {
         StringBuilder message = new StringBuilder(Emulator.getTexts().getValue("commands.generic.cmd_commands.text"));
         List<Command> commands = Emulator.getGameEnvironment().getCommandHandler().getCommandsForRank(gameClient.getHabbo().getHabboInfo().getRank().getId());
-        message.append("(").append(commands.size()).append("):\r\n");
+        message.append("(").append(commands.size()).append("):\r\n\r\n");
+
+        java.util.TreeMap<Integer, java.util.List<Command>> categorized = new java.util.TreeMap<>();
 
         for (Command c : commands) {
-            message.append(Emulator.getTexts().getValue("commands.description." + c.permission, "commands.description." + c.permission)).append("\r");
+            int minRank = 1;
+            // Buscar el rango mínimo que tiene este permiso habilitado
+            for (int r = 1; r <= 15; r++) {
+                if (Emulator.getGameEnvironment().getPermissionsManager().rankExists(r)) {
+                    com.eu.habbo.habbohotel.permissions.Permission p = Emulator.getGameEnvironment().getPermissionsManager().getRank(r).getPermissions().get(c.permission);
+                    if (p != null && p.setting != com.eu.habbo.habbohotel.permissions.PermissionSetting.DISALLOWED) {
+                        minRank = r;
+                        break;
+                    }
+                }
+            }
+            categorized.putIfAbsent(minRank, new java.util.ArrayList<>());
+            categorized.get(minRank).add(c);
+        }
+
+        for (java.util.Map.Entry<Integer, java.util.List<Command>> entry : categorized.entrySet()) {
+            String rankName = Emulator.getGameEnvironment().getPermissionsManager().getRank(entry.getKey()).getName();
+            message.append("--- Categoría: ").append(rankName).append(" ---\r\n");
+            for (Command c : entry.getValue()) {
+                String cmdName = ":" + c.keys[0];
+                String description = Emulator.getTexts().getValue("commands.description." + c.permission, "Sin descripción");
+                message.append(cmdName).append(" - ").append(description).append("\r\n");
+            }
+            message.append("\r\n");
         }
 
         gameClient.getHabbo().alert(new String[]{message.toString()});
